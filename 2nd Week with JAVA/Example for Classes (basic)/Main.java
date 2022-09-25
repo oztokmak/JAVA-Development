66fdd92945d1ca441233bdd4f8d437c27da157a8
Ömer Turan ÖZTOKMAK <65924870+turanoztokmak@users.noreply.github.com>
2022-09-26 00:49:17 +0300

    Example For Classes (Basic)

---
+++ 2nd Week with JAVA/Example for Classes (basic)/Main.java
@@ -0,0 +1,13 @@
+package Main;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		calculator calculator =new calculator();
+		int sonuc = calculator.Topla(7, 6);
+		System.out.println (sonuc);
+
+	}
+	}	
+
